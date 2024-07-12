8b26eefb107c16ae747114f545e26441a1438a1b
Aneesh <67792378+aneesh-oss@users.noreply.github.com>
2024-07-12 12:43:43 +0530

    Create anee.js

---
+++ assign/anee.js
@@ -0,0 +1,19 @@
+mongodb+srv://aneeshNODE_ENV = development
+PORT =5000
+URI =http://localhost:3000
+NEW_RELIC_APPLICATION_LOGGING_FORWARDING_ENABLED=true
+
+# URI =https://blog-jet-mu-94.vercel.app/
+MONGO_URI =mongodb+srv://aneesh:aneesh123@blogstorage.m55c7no.mongodb.net/blogStorage?retryWrites=true&w=majority&appName=blogStorage
+# NEW_RELIC_APP_NAME=BlogBackend NEW_RELIC_LICENSE_KEY=abdcdddf879f833d90965e363b9e4d68FFFFNRAL node -r newrelic server.js
+# NEW_RELIC_APP_NAME=BlogBackend NEW_RELIC_LICENSE_KEY=abdcdddf879f833d90965e363b9e4d68FFFFNRAL node --experimental-loader=newrelic/esm-loader.mjs YOUR_MAIN_FILENAME.js
+JWT_SECRET_KEY = secret
+JWT_EXPIRE = 60m
+RESET_PASSWORD_EXPIRE = 3600000 
+
+# Nodemailer
+
+SMTP_HOST =smtp.gmail.com
+SMTP_PORT =587
+EMAIL_USERNAME = anie81008@gmail.com
+EMAIL_PASS = 12345
